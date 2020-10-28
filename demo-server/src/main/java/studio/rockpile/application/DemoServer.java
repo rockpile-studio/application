9dad5745a6c14bcc83b0ad3264e62a89c97427dd
linlei <llei1@linewell.com>
2020-10-28 14:33:49 +0800

    提交更新

--- demo-server/src/main/java/studio/rockpile/application/DemoServer.java
+++ demo-server/src/main/java/studio/rockpile/application/DemoServer.java
@@ -15,14 +15,14 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 // 运行成功后，直接访问 http://127.0.0.1:8848/nacos（默认账号/密码：nacos/nacos）
 
 // 启动sentinel：sentinel-dashboard-1.7.0.jar
-// java -Xms128m -Xmx128m -Dlogging.file=./log/sentinel-dash.log -jar sentinel-dashboard.jar --spring.config.additional-location=./conf/
+// java -Xms128m -Xmx128m -Dserver.port=8090 -Dlogging.file=./log/sentinel-dash.log -jar sentinel-dashboard.jar --spring.config.additional-location=./conf/
 // 配置 application-runtime.properties
 // auth.username=sentinel
 // auth.password=sentinel
 // spring.cloud.nacos.config.server-addr=127.0.0.1:8848
 // spring.cloud.nacos.config.namespace=  # namespace空，表示使用默认的命名空间
 // spring.cloud.nacos.config.group=SENTINEL_GROUP
-// 运行成功后，直接访问 http://127.0.0.1:8080（默认账号/密码：sentinel/sentinel）
+// 运行成功后，直接访问 http://127.0.0.1:8090（默认账号/密码：sentinel/sentinel）
 @SpringBootApplication
 @EnableDiscoveryClient  
 @MapperScan("studio.rockpile.application.**.dao*")
