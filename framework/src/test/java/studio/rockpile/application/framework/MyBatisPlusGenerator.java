81de9b0fa74d4e1123f010ad12a449866b42e33d
linlei <llei1@linewell.com>
2021-01-11 10:34:41 +0800

    提交更新

--- framework/src/test/java/studio/rockpile/application/framework/MyBatisPlusGenerator.java
+++ framework/src/test/java/studio/rockpile/application/framework/MyBatisPlusGenerator.java
@@ -18,75 +18,75 @@ import com.baomidou.mybatisplus.generator.config.rules.NamingStrategy;
 
 public class MyBatisPlusGenerator {
 
-	public static void main(String[] args) throws SQLException {
-		GlobalConfig globalConf = new GlobalConfig();
-		globalConf.setActiveRecord(false);
-		globalConf.setAuthor("rockpile");
-		globalConf.setOutputDir("E:\\server\\temporary\\mybatis-generator\\src");
-		globalConf.setSwagger2(true);
-		globalConf.setFileOverride(true);
-		globalConf.setIdType(IdType.ASSIGN_ID); // 主键生成策略
-		globalConf.setServiceName("%sProvider");
-		globalConf.setServiceImplName("%sProviderImpl");
-		globalConf.setEnableCache(false); // 是否开启二级缓存，只适合数据量小更新少的场景
-		globalConf.setBaseResultMap(true);
-		globalConf.setBaseColumnList(true);
+    public static void main(String[] args) throws SQLException {
+        GlobalConfig globalConf = new GlobalConfig();
+        globalConf.setActiveRecord(false);
+        globalConf.setAuthor("rockpile");
+        globalConf.setOutputDir("F:/workstation/temporary/mybatis-generator/src");
+        globalConf.setSwagger2(true);
+        globalConf.setFileOverride(true);
+        globalConf.setIdType(IdType.ASSIGN_ID); // 主键生成策略
+        globalConf.setServiceName("%sService");
+        globalConf.setServiceImplName("%sServiceImpl");
+        globalConf.setEnableCache(false); // 是否开启二级缓存，只适合数据量小更新少的场景
+        globalConf.setBaseResultMap(true);
+        globalConf.setBaseColumnList(true);
 
-		DataSourceConfig dsConf = new DataSourceConfig();
-		dsConf.setDbType(DbType.MYSQL);
-		dsConf.setDriverName("com.mysql.cj.jdbc.Driver");
-		dsConf.setUrl(
-				"jdbc:mysql://192.168.4.119:3301/mystudio?useUnicode=true&characterEncoding=UTF-8&useSSL=false&zeroDateTimeBehavior=convertToNull");
-		dsConf.setUsername("rockpile");
-		dsConf.setPassword("rockpile");
-		dsConf.setTypeConvert(new MySqlTypeConvert() {
-			// 自定义数据库表字段类型转换
-			@Override
-			public DbColumnType processTypeConvert(GlobalConfig globalConfig, String fieldType) {
-				if (fieldType.toLowerCase().contains("tinyint")) {
-					return DbColumnType.BOOLEAN;
-				} else if (fieldType.toLowerCase().contains("datetime")) {
-					return DbColumnType.DATE;
-				} else if (fieldType.toLowerCase().contains("double")) {
-					return DbColumnType.BIG_DECIMAL;
-				} else if (fieldType.toLowerCase().contains("json")) {
-					return DbColumnType.STRING;
-				} else {
-					return (DbColumnType) super.processTypeConvert(globalConfig, fieldType);
-				}
-			}
-		});
+        DataSourceConfig dsConf = new DataSourceConfig();
+        dsConf.setDbType(DbType.MYSQL);
+        dsConf.setDriverName("com.mysql.cj.jdbc.Driver");
+        dsConf.setUrl(
+                "jdbc:mysql://192.168.209.130:3301/mystudio?useUnicode=true&characterEncoding=UTF-8&useSSL=false&zeroDateTimeBehavior=convertToNull");
+        dsConf.setUsername("rockpile");
+        dsConf.setPassword("rockpile");
+        dsConf.setTypeConvert(new MySqlTypeConvert() {
+            // 自定义数据库表字段类型转换
+            @Override
+            public DbColumnType processTypeConvert(GlobalConfig globalConfig, String fieldType) {
+                if (fieldType.toLowerCase().contains("tinyint")) {
+                    return DbColumnType.BOOLEAN;
+                } else if (fieldType.toLowerCase().contains("datetime")) {
+                    return DbColumnType.DATE;
+                } else if (fieldType.toLowerCase().contains("double")) {
+                    return DbColumnType.BIG_DECIMAL;
+                } else if (fieldType.toLowerCase().contains("json")) {
+                    return DbColumnType.STRING;
+                } else {
+                    return (DbColumnType) super.processTypeConvert(globalConfig, fieldType);
+                }
+            }
+        });
 
-		String[] tableNames = { "t_account", "t_order", "t_dict", "t_payment" };
+        String[] tableNames = {"t_account"};
 
-		StrategyConfig strategyConf = new StrategyConfig();
-		strategyConf.setNaming(NamingStrategy.underline_to_camel);
-		strategyConf.setColumnNaming(NamingStrategy.underline_to_camel);
-		strategyConf.setEntityLombokModel(false);
-		strategyConf.setRestControllerStyle(true);
-		strategyConf.setTablePrefix("t_");
-		strategyConf.setInclude(tableNames);
-		// strategyConf.setSuperEntityClass(BaseEntity.class.getName());
-		// strategyConf.setSuperEntityColumns(new String[] { "id", "is_del" });
-		strategyConf.setSuperMapperClass(BaseMapper.class.getName());
-		strategyConf.setSuperServiceClass(IService.class.getName());
-		strategyConf.setSuperServiceImplClass(ServiceImpl.class.getName());
+        StrategyConfig strategyConf = new StrategyConfig();
+        strategyConf.setNaming(NamingStrategy.underline_to_camel);
+        strategyConf.setColumnNaming(NamingStrategy.underline_to_camel);
+        strategyConf.setEntityLombokModel(false);
+        strategyConf.setRestControllerStyle(true);
+        strategyConf.setTablePrefix("t_");
+        strategyConf.setInclude(tableNames);
+        // strategyConf.setSuperEntityClass(BaseEntity.class.getName());
+        // strategyConf.setSuperEntityColumns(new String[] { "id", "is_del" });
+        strategyConf.setSuperMapperClass(BaseMapper.class.getName());
+        strategyConf.setSuperServiceClass(IService.class.getName());
+        strategyConf.setSuperServiceImplClass(ServiceImpl.class.getName());
 
-		PackageConfig packageConf = new PackageConfig();
-		packageConf.setParent("studio.rockpile.application");
-		packageConf.setMapper("server.demo.dao");
-		packageConf.setService("server.demo.provider");
-		packageConf.setServiceImpl("server.demo.provider.impl");
-		packageConf.setController("server.demo.controller");
-		packageConf.setEntity("model.entity");
-		packageConf.setXml("server.demo.dao.mapper");
+        PackageConfig packageConf = new PackageConfig();
+        packageConf.setParent("studio.rockpile.application");
+        packageConf.setMapper("server.demo.dao");
+        packageConf.setService("server.demo.provider");
+        packageConf.setServiceImpl("server.demo.provider.impl");
+        packageConf.setController("server.demo.controller");
+        packageConf.setEntity("model.entity");
+        packageConf.setXml("server.demo.dao.mapper");
 
-		AutoGenerator generator = new AutoGenerator();
-		generator.setGlobalConfig(globalConf);
-		generator.setDataSource(dsConf);
-		generator.setStrategy(strategyConf);
-		generator.setPackageInfo(packageConf);
+        AutoGenerator generator = new AutoGenerator();
+        generator.setGlobalConfig(globalConf);
+        generator.setDataSource(dsConf);
+        generator.setStrategy(strategyConf);
+        generator.setPackageInfo(packageConf);
 
-		generator.execute();
-	}
+        generator.execute();
+    }
 }
